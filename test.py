eb35223113f804448a5eeab992a9d33c98a29425
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-08-30 01:12:40 +0530

    feat: add project structure

---
+++ test.py
@@ -0,0 +1,48 @@
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+from backtesting.test import SMA, GOOG
+class SmaCross(Strategy):
+    def init(self):
+        price = self.data.Close
+        self.ma1 = self.I(SMA, price, 10)
+        self.ma2 = self.I(SMA, price, 20)
+        indicators = [a,b,c]
+        for indicator in []:
+            self.indicator = self.I(indicator, price)
+        
+        
+    def next(self):
+        if crossover(self.ma1, self.ma2):
+            self.buy()
+        elif crossover(self.ma2, self.ma1):
+            self.sell()
+            
+            
+bt = Backtest(GOOG, SmaCross, commission=.002,
+              exclusive_orders=True)
+stats = bt.run()
+
+
+{
+    1: 100, 
+    2:200,
+    3:300
+    
+    
+    
+}....
+
+
+{
+    1: 150,
+    2:200,
+    3:300
+    
+    
+    
+}....
+
+
+ma1 , ma2 , data.close 
+
+😍
